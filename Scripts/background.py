ceca9814dbf38235e07b2c9afd4c2009ce4a7996
Xinxin Liu <xxliu95@gmail.com>
2019-06-14 17:11:31 +0200

    14-06-2019

--- Scripts/background.py
+++ Scripts/background.py
@@ -7,13 +7,11 @@ import argparse
 from xml.dom.minidom import parseString
 
 def parse_args():
-	parser = argparse.ArgumentParser(description='Selecciona metodos de una lista pasada como argumento.', add_help=False)
+	parser = argparse.ArgumentParser(description='Busca metodos de una lista pasada como argumento.', add_help=False)
 	parser.add_argument('--apk', '-a', help='Nombre del apk', required=True)
-	parser.add_argument('--methods', '-m', help='Tipos de metodos buscados, pueden ser camara, localizacion o audio. Si no pasa este argumento se analiza todos.')
-	parser.add_argument('--permission', '-p', action='store_true', help='Extrae solo los permisos')
-	parser.add_argument('--feature', '-f', action='store_true', help='Extrae solo las caracteristicas')
-	parser.add_argument('--service', '-s', action='store_true', help='Extrae solo los servicios')
-	parser.add_argument('--receiver', '-r', action='store_true', help='Extrae solo los Broadcast Receiver')
+	parser.add_argument('--methods', '-m', help='Ruta de un fichero JSON con metodos que se quiere buscar. Si no pasa este argumento se analiza todos.')
+	parser.add_argument('--permission', '-p', action='store_true', help='Extrae los permisos')
+	parser.add_argument('--feature', '-f', action='store_true', help='Extrae las caracteristicas')
 
 	parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS,
                     	help='Muestra este mensaje de ayuda.')
@@ -21,17 +19,18 @@ def parse_args():
 
 
 ### Buscando en el manisfest
-def searchPermissions(apk, file):
+def searchPermissions(apk):
 
 	data = ''
 	pathManifest = '../report/{}/filesExtracted/AndroidManifest.xml'.format(apk)
+	file = "../report/{}/permissions.csv".format(apk)
 
 	with open(pathManifest,'r') as f:
 	    data = f.read()
 	dom = parseString(data)
 	nodes = dom.getElementsByTagName('uses-permission')
 
-	with open(file, 'a', encoding='utf-8') as f:
+	with open(file, 'w', encoding='utf-8') as f:
 		w = csv.writer(f)
 
 		w.writerow(["Permisos"])
@@ -41,17 +40,18 @@ def searchPermissions(apk, file):
 		
 		w.writerow([])
 
-def searchFeatures(apk, file):
+def searchFeatures(apk):
 
 	data = ''
 	pathManifest = '../report/{}/filesExtracted/AndroidManifest.xml'.format(apk)
+	file = "../report/{}/features.csv".format(apk)
 
 	with open(pathManifest,'r') as f:
 	    data = f.read()
 	dom = parseString(data)
 	nodes = dom.getElementsByTagName('uses-feature')
 
-	with open(file, 'a', encoding='utf-8') as f:
+	with open(file, 'w', encoding='utf-8') as f:
 		w = csv.writer(f)
 
 		w.writerow(["Caracteristicas"])
@@ -61,35 +61,36 @@ def searchFeatures(apk, file):
 		
 		w.writerow([])
 
-def searchServices(apk, file):
+def searchServices(apk):
 
 	data = ''
 	pathManifest = '../report/{}/filesExtracted/AndroidManifest.xml'.format(apk)
+	file = "../report/{}/ServicesAndReceivers.csv".format(apk)
 
 	with open(pathManifest,'r') as f:
 	    data = f.read()
+
+
 	dom = parseString(data)
 	nodes = dom.getElementsByTagName('service')
 
-	with open(file, 'a', encoding='utf-8') as f:
+	with open(file, 'w', encoding='utf-8') as f:
 		w = csv.writer(f)
 
-		w.writerow(["Servicios", "Permisos adicionales necesarios para el servicio"])
 		# Iterate over all the service nodes
 		for node in nodes:
-			if node.getAttribute('android:permission'):
-				w.writerow([node.getAttribute('android:name'), node.getAttribute('android:permission')])
+			if node.getAttribute('android:permission') == 'android.permission.BIND_JOB_SERVICE':
+				w.writerow(["Job service", node.getAttribute('android:name')])
 			else:
-				w.writerow([node.getAttribute('android:name'), 'ninguno'])
+				w.writerow(["Service", node.getAttribute('android:name')])
 		
-		w.writerow([])
 
-
-def searchBroadcastReceiver(apk, file):
+def searchBroadcastReceiver(apk):
 
 	data = ''
-	#with open('../1 - ExtractedApks/{}/AndroidManifest.xml'.format(apk),'r') as f:
 	pathManifest = '../report/{}/filesExtracted/AndroidManifest.xml'.format(apk)
+	file = "../report/{}/ServicesAndReceivers.csv".format(apk)
+
 	with open(pathManifest,'r') as f:
 	    data = f.read()
 
@@ -100,64 +101,51 @@ def searchBroadcastReceiver(apk, file):
 	with open(file, 'a', encoding='utf-8') as f:
 		w = csv.writer(f)
 
-		w.writerow(["Broadcast Receiver"])
 		# Iterate over all the service nodes
 		for node in nodes:
-				w.writerow([node.getAttribute('android:name')])
+				w.writerow(["Receiver", node.getAttribute('android:name')])
 		
-		w.writerow([])
-
 def extractApk(apk):
 	os.system('apktool d ../apks/{}.apk -o ../report/{}/filesExtracted'.format(apk, apk))
 
 def callgraph(apk, file, methods):
 	#os.system('source venv-androguard/bin/activate')
-	if methods == "localizacion":
-		os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods config-files/METODOS_LOCALIZACION.json --save-csv -c importantMethods'.format(apk))
-	elif methods == "camara":
-		os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods config-files/METODOS_CAMARA.json --save-csv -c importantMethods'.format(apk))
-	elif methods == "audio":
-		os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods config-files/METODOS_AUDIO.json --save-csv -c importantMethods'.format(apk))
-	else:
-		os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods config-files/METODOS.json --save-csv -c importantMethods'.format(apk))
+	os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods {} --save-csv -c importantMethods'.format(apk, methods))
+	#os.system('python callgraphBIEN.py -a /home/static/analysis/apks/{}.apk --methods {} --save-csv --verbose -c importantMethods'.format(apk, methods))
 
 	with open('../report/{}/importantMethods_{}.csv'.format(apk, apk), 'r') as cg:
 		reader = csv.reader(cg)
+		next(reader)
+		lista_metodos = []
+		for line in reader:
+			metodo = line[0].strip().replace("/",".").replace(";","")[3:-1] 
+			utilizado = line[1].strip().replace("/",".").replace(";","")[3:-1]
+			lista_metodos.append([metodo, utilizado])
 
-		with open(file, 'a', encoding='utf-8') as f:
+	with open('../report/{}/ServicesAndReceivers.csv'.format(apk), 'r') as sr:
+		readersr = csv.reader(sr)
+		with open(file, 'w', encoding='utf-8') as f:
 			writer = csv.writer(f)
+			writer.writerow(["Tipo", "Clase", "Metodo"])
 
-			for line in reader:
-				writer.writerow(line)
+			# Busca si en la lista de metodos si esta en un servicio
+			for l in readersr:
+				for linea in lista_metodos:
+					if linea[1] == l[1]:
+						writer.writerow([l[0], l[1], linea[0]])
 
-def init(file):
-	with open(file, 'w', encoding='utf-8') as f:
-		w = csv.writer(f)
-
-		w.writerow(["Resultado de analisis de la app {}.apk".format(apk)])
-		w.writerow([])
-
-def analyze(apk,  methods, permission=False, service=False, receiver=False, feature=False):
+def analyze(apk,  methods, permission=False, feature=False):
 
 	file = "../report/{}/report.csv".format(apk)
 
-	init(file)
-
 	if permission:
-		searchPermissions(apk, file)
-	elif service:
-		searchServices(apk, file)
-	elif receiver:
-		searchBroadcastReceiver(apk, file)
-	elif feature:
-		searchFeatures(apk, file)
-	else:
-		searchPermissions(apk, file)
-		searchFeatures(apk, file)
-		searchServices(apk, file)
-		searchBroadcastReceiver(apk,file)
-		callgraph(apk, file, methods)
-
+		searchPermissions(apk)
+	if feature:
+		searchFeatures(apk)
+	
+	searchServices(apk)
+	searchBroadcastReceiver(apk)
+	callgraph(apk, file, methods)
 
 if __name__ == '__main__':
 
@@ -166,8 +154,14 @@ if __name__ == '__main__':
 	apk = args.apk
 	methods = args.methods
 	
-	if not os.path.exists('../report/{}/filesExtracted'.format(apk)):
-		extractApk(apk)
+	if args.methods is not None:
+		methods = args.methods
+	else:
+		methods = "config-files/METODOS.json"
 
-	analyze(apk, methods, args.permission, args.service, args.receiver, args.feature)
+	#if not os.path.exists('../report/{}/filesExtracted'.format(apk)):
+	#	extractApk(apk)
+		
+	analyze(apk, methods, args.permission, args.feature)
+
 	
